f7c95b61430b4ddc09b206d3f638975f887f8dae
2017fandrei <fandrei2000@yahoo.com>
2025-01-22 18:02:34 +0200

    adds homework 5

---
+++ homework5.py
@@ -0,0 +1,14 @@
+"""
+Define a list of 10 items.
+
+1. Search for an item in a list using a for loop.
+2. Calculate and display the sum of the elements in a list using a for loop.
+3. Display the first 3 items in a list using slicing.
+4. Display items in a list from middle to end using slicing.
+5. Display items in a list at intervals of 2 using slicing.
+6. Display inverted list using slicing.
+7. Display items in a list from index 1 to 4 using slicing.
+8. Display the last 3 items in a list using slicing.
+9. Display the items in a list from the beginning to the penultimate item using slicing.
+10. Display only items greater than 3 in a list using a for loop.
+"""
